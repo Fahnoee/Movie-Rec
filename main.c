b125aa254fa28c8865d3bf34eda417c9415f175e
Mikkel Martinus Fahnøe <mikkel@h396.aau1x-2.klient.sydhavn2.site.aau.dk>
2023-11-29 13:59:42 +0100

    Oprettede adjust service og is element in array functions

--- main.c
+++ main.c
@@ -6,15 +6,16 @@
 
 void main(void)
 {
+    int available_s[11] = {1,1,1,1,1,1,1,1,1,1,1};
 }
 
 // main menu
 void printMenu() {
     // Integer value for selecting menu option
     int selection;
-    int (*functionArray[])() = {f1, f2, adjust_s_services, f4};
+    int (*arrayOfFunctions[])() = {f1, f2, adjust_s_services, f4};
     int menuOption[] = {1, 2, 3};
-    int menuOptionCount = 3
+
 
     // Print menu options
     printf("== MENU ==\n");
@@ -24,15 +25,32 @@ void printMenu() {
     
     
     
-    scanf("%d", &selection)
-    for (int i = 0; i < menuOptionCount; i++) {
-        if (selection == menuOption[0])
+    scanf("%d", &selection);
+    
+    if (is_element_in_array(selection, menuOption)) {
+        arrayOfFunctions[selection]()
+    } else {
+        printf("Invalid input!")
     }
-    
+} 
 
-    
-
+// Function to check if x is in array
+int is_element_in_array(int x, int arr[]) {
+    int arrayLength = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < arrayLength; i++) {
+        if (arr[i] == x) {
+            return 1;  // x found
+        }
+    }
+    return 0;  // x not found
 }
+
+// Function for adjusting available streaming services
+void adjust_s_services(int * available_s) {
+    printf("Currently yo");
+}
+
+
 
 
 // datasaet
