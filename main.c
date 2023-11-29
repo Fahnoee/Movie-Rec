a2182f6be0564b756770abc62dd9016f61228b09
Mikkel Martinus Fahnøe <mikkel@h396.aau1x-2.klient.sydhavn2.site.aau.dk>
2023-11-29 14:19:59 +0100

    Skrev videre paa justering af service

--- main.c
+++ main.c
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+
+typedef struct {
+    int available_s[11];
+} Settings;
+
+
 // prototyper
 
-void main(void)
-{
-    int available_s[11] = {1,1,1,1,1,1,1,1,1,1,1};
+void main(void) {
+    settings mySettings = {
+        .available_s = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1} 
+    }
 }
 
 // main menu
@@ -46,9 +54,81 @@ int is_element_in_array(int x, int arr[]) {
 }
 
 // Function for adjusting available streaming services
-void adjust_s_services(int * available_s) {
-    printf("Currently yo");
+void print_services(int * available_s) {
+    printf("\nCurrently you have the following streaming services available:\n");
+    
+    // Print available streaming services and whether you have them or not
+    if (available_s[0] == 1){
+    printf("0: Active: Netflix\n")}
+    else {"0: Not active: Netflix\n"}
+    
+    if (available_s[1] == 1){
+    printf("1: Active: DRTV\n")}
+    else {"1: Not active: DRTV\n"}
+
+    if (available_s[2] == 1){
+    printf("2: Active: HBO Max\n")}
+    else {"2: Not active: HBO max \n"}
+    
+    if (available_s[3] == 1){
+    printf("3: Active: Disney+\n")}
+    else {"3: Not active: Disney+\n"} 
+
+    if (available_s[4] == 1){
+    printf("4: Active: TV2 Play\n")}
+    else {"4: Not active: TV2 Play\n"}
+
+    if (available_s[5] == 1){
+    printf("5: Active: SkyShowtime\n")}
+    else {"5: Not active: SkyShowtime\n"}
+
+    if (available_s[6] == 1){
+    printf("6: Active: Viaplay\n")}
+    else {"6: Not active: Viaplay\n"}
+
+    if (available_s[7] == 1){
+    printf("7: Active: Film Striben\n")}
+    else {"7: Not active: Film Striben\n"}
+
+    if (available_s[8] == 1){
+    printf("8: Active: C-More\n")}
+    else {"8: Not active: C-more\n"}
+    
+    if (available_s[9] == 1){
+    printf("9: Active: Amazon Prime\n")}
+    else {"9: Not active: Amazon Prime\n"}
+
+    if (available_s[10] == 1){
+    printf("10: Active: Rakuten\n")}
+    else {"10: Not active: Rakuten\n"}
+
+    int number_of_choice = 0;
+    // Ask user which streaming service they want to activate/deactivate
+    printf("\nWhich streaming service do you want to activate/deactivate?\n");
+    printf("If you dont wanna change any press -1.\n");
+    printf("Enter number:")
+
+    scanf("%d", &number_of_choice);
+    
+    if (number_of_choice==-1){
+        //to go main menu
+        printMenu();
+    }
+    else if (number_of_choice>=0 && number_of_choice<=10){
+        //change the value of the streaming service
+
+        //to go main menu
+        printMenu();
+    }
+    else {
+        printf("Invalid input! We try again\n")
+        //to go main menu
+
+        adjust_s_services(available_s);
+    }
+    
 }
+
 
 
 
