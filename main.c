28bb2cded5cd97c0a24eee22f4aec551d524ac35
Mikkel Martinus Fahnøe <mikkel@h396.aau1x-2.klient.sydhavn2.site.aau.dk>
2023-11-29 13:46:37 +0100

    Lavede Print for menu

--- main.c
+++ main.c
@@ -9,5 +9,30 @@ void main(void)
 }
 
 // main menu
+void printMenu() {
+    // Integer value for selecting menu option
+    int selection;
+    int (*functionArray[])() = {f1, f2, adjust_s_services, f4};
+    int menuOption[] = {1, 2, 3};
+    int menuOptionCount = 3
+
+    // Print menu options
+    printf("== MENU ==\n");
+    printf("1: Get a recommendation\n");
+    printf("2: Adjust your streaming services\n");
+    printf("3: EXIT\n");
+    
+    
+    
+    scanf("%d", &selection)
+    for (int i = 0; i < menuOptionCount; i++) {
+        if (selection == menuOption[0])
+    }
+    
+
+    
+
+}
+
 
 // datasaet
