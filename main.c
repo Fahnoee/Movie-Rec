9d025c2e94f0eb5aef7979e500105a6f9edf3fc0
magnusew <magnuswich@live.dk>
2023-11-29 20:59:19 +0100

    Har fixet en del af kode, manglende semikolon osv.

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ struct genre{
     int animation;
     int biography;
     int history;
-    int sci-fi;
+    int sci_fi;
     int war;
     int family;
     int thriller;
@@ -62,7 +62,7 @@ struct movie{
 //Structs
 typedef struct {
     int available_s[11];
-} Settings;
+} settings;
 
 
 // prototyper
@@ -72,7 +72,9 @@ void print_services(int * available_s);
 void select_change(available_s);
 void printMenu();
 int is_element_in_array(int x, int arr[]);
-void import_movies(int movie_array[]);
+void import_movies(struct movie movie_array[]);
+ 
+
 
 //////////////
 /////MAIN/////
@@ -106,9 +108,10 @@ void printMenu() {
     scanf("%d", &selection);
     
     if (is_element_in_array(selection, menuOption)) {
-        arrayOfFunctions[selection]()
-    } else {
-        printf("Invalid input!")
+        arrayOfFunctions[selection]();
+    } 
+    else{
+        printf("Invalid input!");
     }
 } 
 
@@ -126,8 +129,8 @@ int is_element_in_array(int x, int arr[]) {
 
 // Function for adjusting available streaming services
 void adjust_s_services(int * available_s){
-    print_services(available_s)
-    select_change(available_s)
+    print_services(available_s);
+    select_change(available_s);
 
     printMenu();
 }
@@ -137,59 +140,60 @@ void print_services(int * available_s) {
     printf("\nCurrently you have the following streaming services available:\n");
     
     // Print available streaming services and whether you have them or not
+
     if (available_s[0] == 1){
-    printf("0: Active: Netflix\n")}
-    else {"0: Not active: Netflix\n"}
+    printf("0: Active: Netflix\n");}
+    else {("0: Not active: Netflix\n");}
     
     if (available_s[1] == 1){
-    printf("1: Active: DRTV\n")}
-    else {"1: Not active: DRTV\n"}
+    printf("1: Active: DRTV\n");}
+    else {("1: Not active: DRTV\n");}
 
     if (available_s[2] == 1){
-    printf("2: Active: HBO Max\n")}
-    else {"2: Not active: HBO max \n"}
+    printf("2: Active: HBO Max\n");}
+    else {("2: Not active: HBO max \n");}
     
     if (available_s[3] == 1){
-    printf("3: Active: Disney+\n")}
-    else {"3: Not active: Disney+\n"} 
+    printf("3: Active: Disney+\n");}
+    else {("3: Not active: Disney+\n");} 
 
     if (available_s[4] == 1){
-    printf("4: Active: TV2 Play\n")}
-    else {"4: Not active: TV2 Play\n"}
+    printf("4: Active: TV2 Play\n");}
+    else {("4: Not active: TV2 Play\n");}
 
     if (available_s[5] == 1){
-    printf("5: Active: SkyShowtime\n")}
-    else {"5: Not active: SkyShowtime\n"}
+    printf("5: Active: SkyShowtime\n");}
+    else {("5: Not active: SkyShowtime\n");}
 
     if (available_s[6] == 1){
-    printf("6: Active: Viaplay\n")}
-    else {"6: Not active: Viaplay\n"}
+    printf("6: Active: Viaplay\n");}
+    else {("6: Not active: Viaplay\n");}
 
     if (available_s[7] == 1){
-    printf("7: Active: Film Striben\n")}
-    else {"7: Not active: Film Striben\n"}
+    printf("7: Active: Film Striben\n");}
+    else {("7: Not active: Film Striben\n");}
 
     if (available_s[8] == 1){
-    printf("8: Active: C-More\n")}
-    else {"8: Not active: C-more\n"}
+    printf("8: Active: C-More\n");}
+    else {("8: Not active: C-more\n");}
     
     if (available_s[9] == 1){
-    printf("9: Active: Amazon Prime\n")}
-    else {"9: Not active: Amazon Prime\n"}
+    printf("9: Active: Amazon Prime\n");}
+    else {("9: Not active: Amazon Prime\n");}
 
     if (available_s[10] == 1){
-    printf("10: Active: Rakuten\n")}
-    else {"10: Not active: Rakuten\n"}
+    printf("10: Active: Rakuten\n");}
+    else {("10: Not active: Rakuten\n");}
 
-    int number_of_choice = 0;
 }
 
 void select_change(available_s){
     // Ask user which streaming service they want to activate/deactivate
     printf("\nWhich streaming service do you want to activate/deactivate?\n");
     printf("If you dont wanna change any press -1.\n");
-    printf("Enter number:\n")
+    printf("Enter number:\n");
 
+    int number_of_choice = 0;
     scanf("%d", &number_of_choice);
     
     if (number_of_choice==-1){
@@ -203,7 +207,7 @@ void select_change(available_s){
         printMenu();
     }
     else {
-        printf("Invalid input! We try again\n")
+        printf("Invalid input! We try again\n");
         //to go main menu
 
         adjust_s_services(available_s);
@@ -215,54 +219,52 @@ void select_change(available_s){
 
 void run()
 {
-    movie_array[MAX_MOVIES];
+    struct movie movie_array[MAX_MOVIES];
 
     import_movies(movie_array);
 }
 
 
-void import_movies(int movie_array[]){
-
+void import_movies(struct movie movie_array[])
+{
     FILE *f = fopen("movies.txt", "r");
-    if (f == NULL){
+    if (f == NULL)
+    {
         printf("Det mislykkedes at aabne tekstfilen.\n");
         exit(EXIT_FAILURE);
     }
-    
-    for(int i = 0; i < MAX_MOVIES; i++){
-        
-        fscanf(f, "%d;%s;%d;%d;%struct;%d;%s;%struct", &movie_array. );
-    }
-    
 
-    f(close);
-}
-
-match import_matches(FILE *f, movie_array[])
-{
-
-    for(int i = 0; i < MAX_GAMES; i++)
+    for(int i = 0; i < MAX_MOVIES; i++)
     {
         fscanf(f, "%d;%s;%d;%d;",  
-                &movie[i].id,
-                movie[i].title,
-                &movie[i].pg,
-                &movie[i].runtime); 
+                &movie_array[i].id,
+                movie_array[i].title,
+                &movie_array[i].pg,
+                &movie_array[i].runtime); 
                 
-        %struct
+        //import genre struct
         
         fscanf(f, ";%d;%s;", 
-        &movie[i].score, movie[i].resume);
-        
-        %struct
+        &movie_array[i].score, movie_array[i].resume);
+    
+        //import service struct
     }
 }
 
 import_genre(FILE *f, int movie_array[]){
-    fscanf(f, %s)
+    char *genre_1[10];
+    char *genre_2[10];
+    char *genre_3[10];
+
+    fscanf(f, "%s, %s, %s", genre_1, genre_2, genre_3 );
+
+
 }
 
 import_services(FILE *f, int movie_array[])
+{
+
+}
 
 
 //Function for welcomming the new user
